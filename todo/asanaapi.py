821cef407d02d2fb818342185d5e2c8375056e24
suvooncode <32808038+suvooncode@users.noreply.github.com>
2023-09-17 22:07:33 +0530

    Update asanaapi.py

--- todo/asanaapi.py
+++ todo/asanaapi.py
@@ -6,7 +6,7 @@ class AsanaApi :
         # Initialize the Asana client with your personal access token
         # client = asana.Client.access_token('')
         configuration = asana.Configuration()
-        configuration.access_token = '1/1205492548060687:1b8e3277f1609e6e8f2128cb505da097'
+        configuration.access_token = '___asnaapi___'
         api_client = asana.ApiClient(configuration)
 
         api_instance = asana.TasksApi(api_client)
